bdba9794fade592c73b345222863eed6151cf7ee
Zazz7res <harrylee_x@outlook.com>
2025-10-18 19:12:15 +0800

    git add by vmMiniPC

---
+++ Chapter4/Exp/talkback.c
@@ -0,0 +1,22 @@
+//  talkback.c  --  演示与用户交互
+#include <stdio.h>
+#include <string.h>
+#define DENSITY 62.4
+
+
+int main(void)
+{
+
+	float weight, volume;
+	int size, letters;
+	char name[40];
+
+
+	printf("Hi! What's your first name?\n");
+
+
+
+	return 0;
+
+
+}
