049e493fa982905ad4f79a1b9ae9186259efb46c
Zazz7res <harrylee_x@outlook.com>
2025-10-29 22:11:31 +0800

    git add by harry

---
+++ Chapter10/Exp/pnt_add.c
@@ -0,0 +1,25 @@
+/*  pnt_add.c  --  指针地址  */
+#include <stdio.h>
+#define SIZE 4
+
+
+int main(void)
+{
+
+    short dates[SIZE];
+    short * pti;
+    short index;
+    double bills[SIZE];
+    double * ptf;
+    pti = dates;
+    ptf = bills;
+    printf("%23s %15s\n", "short", "double");
+    for (index = 0; index < SIZE; index++)
+    {
+        printf("pointers + %d: %10p %15p\n", index, pti + index, ptf + index);
+        
+    }
+
+
+    return 0;
+}
