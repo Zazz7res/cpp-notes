c919f647942d264c5b4f928c1cddc26f1ee5fafb
Zazz7res <harrylee_x@outlook.com>
2025-11-05 22:03:04 +0800

    git add by harrypc

---
+++ Chapter9/hotel/hotel.h
@@ -0,0 +1,15 @@
+/*  hotel.h  --  符号常量和 hotel.c 中所有函数的原型  */
+#define QUIT    5
+#define HOTEL1      180.00
+#define HOTEL2      225.00
+#define HOTEL3      255.00
+#define HOTEL4      355.00
+#define DISCOUNT     0.95
+#define STARS "***********************"
+
+
+int menu(void);
+
+int getnights(void);
+
+void showprice(double rate, int nights);
