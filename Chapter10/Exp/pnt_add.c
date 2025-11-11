8e5779e8a01a398982008486d063f0a740926aff
Zazz7res <harrylee_x@outlook.com>
2025-11-11 16:47:11 +0800

    git add by pad

--- Chapter10/Exp/pnt_add.c
+++ Chapter10/Exp/pnt_add.c
@@ -1,4 +1,5 @@
-/*  pnt_add.c  --  指针地址  */
+/*  pnt_add.c  --  for pnt.c  */
+
 #include <stdio.h>
 #define SIZE 4
 
@@ -15,20 +16,10 @@ int main(void)
     ptf = bills;
     printf("%23s %15s\n", "short", "double");
     for (index = 0; index < SIZE; index++)
-    {
-        printf("pointers + %d: %10p %15p\n", index, pti + index, ptf + index);
+        printf("pointers + %d: %10p %10p\n", index, pti + index, ptf + index);
 
-
-        
-    }
 
 
     return 0;
-}
 
-//double * const pc = rates;  //pc这个指针不允许指向别处
-//但是他可以修改rates数组中的值
-//const double * cp = rates;  //cp这个指针可以指向别处
-//但是不允许通过cp修改rates数组中的值
-//const double * const cpc = rates; //cpc这个指针不允许指
-//向别处,也不允许通过cpc修改rates数组中的值
+}
