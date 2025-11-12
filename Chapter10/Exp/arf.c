040ab2907ad33c8a14110079e78a83fa746145dd
Zazz7res <harrylee_x@outlook.com>
2025-11-12 15:46:24 +0800

    git add by pad

--- Chapter10/Exp/arf.c
+++ Chapter10/Exp/arf.c
@@ -1,4 +1,5 @@
-/*  arf.c  --  for group  */
+/*  arf.c  --  for deal with group  */
+
 #include <stdio.h>
 #define SIZE 5
 void show_array(const double ar[], int n);
@@ -7,43 +8,40 @@ void mult_array(double ar[], int n, double mult);
 
 int main(void)
 {
-
     double dip[SIZE] = { 20.0, 17.66, 8.2, 15.3, 22.22 };
-    
+
     printf("The original dip array:\n");
     show_array(dip, SIZE);
     mult_array(dip, SIZE, 2.5);
     printf("The dip array after calling mult_array():\n");
     show_array(dip, SIZE);
 
-    return 0;
 
+
+    return 0;
 
 }
 
 
 void show_array(const double ar[], int n)
 {
-
     int i;
 
-
     for (i = 0; i < n; i++)
-        printf("%8.3f", ar[i]);
+        printf("%8.3f ", ar[i]);
     putchar('\n');
+
+
 }
+
+
 
 void mult_array(double ar[], int n, double mult)
 {
 
     int i;
-    
 
     for (i = 0; i < n; i++)
         ar[i] *= mult;
-
-
-
-
 
 }
