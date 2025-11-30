232f97ee986c451188a22794ac5e9fafb5204a45
Zazz7res <harrylee_x@outlook.com>
2025-11-30 22:39:16 +0800

    git add by vmHarryPC

--- Chapter06/Exp/for_show.c
+++ Chapter06/Exp/for_show.c
@@ -1,18 +1,12 @@
-//  for_show.c
+/*  for_show.c  */ 
 #include <stdio.h>
 int main(void)
 {
     int num = 0;
 
-
-    for (printf("Keep entering numbers!\n");num != 6;)
+    for (printf("Keep entering number!\n"); num != 6;)
         scanf("%d", &num);
     printf("That's the one I want!\n");
+
     return 0;
-
-
-
-
-
-
 }
