95971e873761acedf57ce695c30f19a68c0c2be6
Zazz7res <harrylee_x@outlook.com>
2025-11-30 17:20:49 +0800

    git add

--- Chapter06/Exp/sweetie1.c
+++ Chapter06/Exp/sweetie1.c
@@ -1,26 +1,14 @@
-//  sweetie1.c  --  a count number program
+//  sweetie1.c  --  一个计数循环
 #include <stdio.h>
 int main(void)
 {
-
     const int NUMBER = 22;
     int count = 1;
 
-
-
-    while (count <= NUMBER)
-    {
-
-        printf("Be my Valentin!\n");
+    while (count <= NUMBER) {
+        printf("Be my Valentine!\n");
         count++;
-
-
-
     }
-
-
-
-
 
     return 0;
 
