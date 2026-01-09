54691c9de46aaa4e004fcd72c9542310ee3fb540
Zazz7res <harrylee_x@outlook.com>
2026-01-10 06:31:35 +0800

    git add

---
+++ Chapter09/Prac/Prac1.c
@@ -0,0 +1,25 @@
+/*
+    1．设计一个函数min(x, y)，返回两个double类型值的较小值。
+    在一个简单的驱动程序中测试该函数。
+*/
+
+#include <stdio.h>
+double min(double, double);
+
+int main(void) {
+    double x, y;
+
+    printf("请输入两个 double 类型的数字：\n");
+    scanf("%lf %lf", &x, &y);
+    printf("这两个数字最小的是：%lf\n", min(x,y));
+
+    return 0;
+}
+
+double min(double x, double y) {
+    if (x < y) {
+        return x;
+    } else {
+        return y;
+    }
+}
